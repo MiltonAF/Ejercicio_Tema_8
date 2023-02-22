695274024de5f0e456eb136aa67b4a342f8819b5
MiltonAF <mafelizzolam@gmail.com>
2023-02-22 18:45:35 -0500

    subiendo

--- EjercicioTema8/src/Main.java
+++ EjercicioTema8/src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona persona = new Persona();
+
+        persona.setEdad(23);
+        persona.setNombre("Max");
+        persona.setTelefono(312456742);
+
+        System.out.println("la persona tiene" + persona.getEdad() + " Años, y su nombre es " + persona.getNombre() + " y su telefono es " + persona.getTelefono());
+
+
+    }
+
+    static class Persona{
+        private int edad;
+        private String nombre;
+        private long telefono;
+
+        public int getEdad() {
+            return edad;
+        }
+
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public long getTelefono() {
+            return telefono;
+        }
+
+        public void setTelefono(long telefono) {
+            this.telefono = telefono;
+        }
     }
 }
